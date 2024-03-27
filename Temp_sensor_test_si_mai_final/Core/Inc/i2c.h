b9bb4ac5eed1154ba277419576e8c083e1bd7e5d
Vladimir Frunza <127752888+VladyFrunzus@users.noreply.github.com>
2024-03-27 03:35:05 +0200

    Add files via upload

---
+++ Temp_sensor_test_si_mai_final/Core/Inc/i2c.h
@@ -0,0 +1,31 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __I2C_H
+#define __I2C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern I2C_HandleTypeDef hi2c1;
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_I2C1_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __I2C_H */
